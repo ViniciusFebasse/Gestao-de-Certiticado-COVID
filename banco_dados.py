fbe3ad015cab90ed77d2cf59dd7db8fc2a768007
ViniciusFebasse <vinicius_serra_@hotmail.com>
2021-12-27 20:55:47 -0300

    Alterações nas funções de banco de dados

--- banco_dados.py
+++ banco_dados.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 
 class BancoDados:
     def __init__(self):
@@ -8,29 +9,29 @@ class BancoDados:
         self.cursor = self.banco.cursor()
 
     def cria(self, nome, telefone):
-        self.cursor.execute(f'''INSERT INTO pessoas (nome, telefone) VALUES('{nome}', '{telefone}')''')
+        self.cursor.execute(f'''INSERT INTO utentes (NOME, TELEMOVEL, NUS) VALUES('{nome}', '{telefone}', {random.randint(0, 999999)})''')
         self.banco.commit()
 
     def consulta(self):
-        self.cursor.execute("SELECT * FROM pessoas")
+        self.cursor.execute("SELECT * FROM utentes")
         print("<<<--->>>")
         for linha in self.cursor.fetchall():
             print(linha)
         print("<<<--->>>")
 
     def mostrar(self):
-        self.cursor.execute("SELECT * FROM pessoas")
+        self.cursor.execute("SELECT * FROM utentes")
         listagem = []
         for linha in self.cursor.fetchall():
             listagem.append(linha)
         return listagem
 
     def apaga(self, nome):
-        self.cursor.execute(f"""DELETE FROM pessoas WHERE nome = '{nome}'""")
+        self.cursor.execute(f"""DELETE FROM utentes WHERE NUS = '{nome}'""")
         self.banco.commit()
 
     def atualiza(self, nome, telefone):
-        self.cursor.execute(f"""UPDATE pessoas SET telefone = '{telefone}' WHERE nome = '{nome}'""")
+        self.cursor.execute(f"""UPDATE utentes SET TELEMOVEL = '{telefone}' WHERE NOME = '{nome}'""")
         self.banco.commit()
 
 
